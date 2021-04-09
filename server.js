1cc84bfd16ed739de13f1ddf2dbbb3ed52eedb56
moti <moti.kor@gmail.com>
2021-04-09 22:05:21 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-const express = require('express')
-const PORT = process.env.PORT || 3000;
-const INDEX = './index.html';
+const { Server } = require('ws');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const wss = new Server({ server });
+
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
