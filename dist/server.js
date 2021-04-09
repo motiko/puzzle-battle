0f30e745674aa84eef8093885b335f50e6cbb0af
moti <moti.kor@gmail.com>
2021-04-09 21:31:32 +0200

    fix

---
+++ dist/server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000;
+const INDEX = 'dist/index.html';
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
